2c1aa5f9f5f0b68fcea1703ef95cf5f8016f802c
Omar <omar.piga@consultant.chili.com>
2022-03-31 16:23:22 +0200

    Import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { chromeExtension } from "rollup-plugin-chrome-extension";
-import manifest from './manifest.json'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { chromeExtension } from 'rollup-plugin-chrome-extension';
+// @ts-ignore
+import manifest from './manifest.json';
+
+const path = require('path');
 
 export default defineConfig({
   plugins: [react(), chromeExtension({ manifest })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
