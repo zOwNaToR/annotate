f68d209acc1c722bae015f2da2daf549db5f0a30
Omar <omar@email.it>
2022-11-21 19:19:27 +0100

    added test for the suite nothing selected

--- src/__test__/commands/WriteCommand.test.ts
+++ src/__test__/commands/WriteCommand.test.ts
@@ -17,19 +17,53 @@ const setup = (options: WriterSetupOptions = getDefaultWriteOptions()) => {
 };
 
 describe('execute method', () => {
-	it('should add text at the begging of first line', () => {
-		const { editorState } = setup();
-		const writeCommand = new WriteCommand(editorState, {
-			nodeKey: 'first',
-			nodeOffset: 0,
-			text: 'Z',
+	describe('nothing selected', () => {
+		it('should add text at the begging of first line', () => {
+			const { editorState } = setup();
+			const writeCommand = new WriteCommand(editorState, {
+				nodeKey: 'first',
+				nodeOffset: 0,
+				text: 'Z',
+			});
+
+			writeCommand.execute();
+
+			const nodesArray = [...editorState.nodes];
+			expect(nodesArray[0][1].text).toBe('ZLorem ipsum');
+			expect(editorState.selection.anchor).toEqual({ key: 'first', offset: 1 });
+			expect(editorState.selection.focus).toEqual({ key: 'first', offset: 1 });
 		});
 
-		writeCommand.execute();
+		it('should add text in middle of second line', () => {
+			const { editorState } = setup();
+			const writeCommand = new WriteCommand(editorState, {
+				nodeKey: 'second',
+				nodeOffset: 5,
+				text: 'Z',
+			});
 
-      const nodesArray = [...editorState.nodes]
-		expect(nodesArray[0][1].text).toBe('ZLorem ipsum');
-		expect(editorState.selection.anchor).toEqual({ key: 'first', offset: 1 });
-		expect(editorState.selection.focus).toEqual({ key: 'first', offset: 1 });
+			writeCommand.execute();
+
+			const nodesArray = [...editorState.nodes];
+			expect(nodesArray[1][1].text).toBe('dolorZ sit');
+			expect(editorState.selection.anchor).toEqual({ key: 'second', offset: 6 });
+			expect(editorState.selection.focus).toEqual({ key: 'second', offset: 6 });
+		});
+
+		it('should add text at the end of first line', () => {
+			const { editorState } = setup();
+			const writeCommand = new WriteCommand(editorState, {
+				nodeKey: 'first',
+				nodeOffset: 11,
+				text: 'Z',
+			});
+
+			writeCommand.execute();
+
+			const nodesArray = [...editorState.nodes];
+			expect(nodesArray[0][1].text).toBe('Lorem ipsumZ');
+			expect(editorState.selection.anchor).toEqual({ key: 'first', offset: 12 });
+			expect(editorState.selection.focus).toEqual({ key: 'first', offset: 12 });
+		});
 	});
 });
