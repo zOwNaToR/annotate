5e062cc81ecdb790f898c655ed6c86f229bd8eea
Omar <omar@email.it>
2022-11-24 19:35:22 +0100

    grouped test in describe and added test: should delete text inserted by execute, restore text and selection on multiple lines (first line completely deleted)

--- src/__test__/commands/write-command/undo.test.ts
+++ src/__test__/commands/write-command/undo.test.ts
@@ -46,41 +46,64 @@ it('should delete text inserted by execute and restore selection Caret', () => {
 	expect(editorState.selection.focus).toEqual({ key: 'first', offset: 0 });
 });
 
-it('should delete text inserted by execute and restore selection Range', () => {
-	const { editorState } = setup();
-	const writeCommand = new WriteCommand(editorState, { text: 'Z' });
-	editorState.selection.set({
-		anchor: { key: 'first', offset: 6 },
-		focus: { key: 'first', offset: 11 },
+describe('selection Range', () => {
+	it('should delete text inserted by execute, restore text and selection', () => {
+		const { editorState } = setup();
+		const writeCommand = new WriteCommand(editorState, { text: 'Z' });
+		editorState.selection.set({
+			anchor: { key: 'first', offset: 6 },
+			focus: { key: 'first', offset: 11 },
+		});
+
+		writeCommand.execute();
+		const executed = writeCommand.undo();
+
+		expect(executed).toBe(true);
+		expect(writeCommand.undoed).toBe(true);
+		expect(editorState.nodes[0].text).toBe('Lorem ipsum');
+		expect(editorState.selection.anchor).toEqual({ key: 'first', offset: 6 });
+		expect(editorState.selection.focus).toEqual({ key: 'first', offset: 11 });
 	});
 
-	writeCommand.execute();
-	const executed = writeCommand.undo();
+	it('should delete text inserted by execute, restore text and selection on multiple lines', () => {
+		const { editorState } = setup();
+		const writeCommand = new WriteCommand(editorState, { text: 'Z' });
+		editorState.selection.set({
+			anchor: { key: 'first', offset: 6 },
+			focus: { key: 'third', offset: 4 },
+		});
 
-	expect(executed).toBe(true);
-	expect(writeCommand.undoed).toBe(true);
-	expect(editorState.nodes[0].text).toBe('Lorem ipsum');
-	expect(editorState.selection.anchor).toEqual({ key: 'first', offset: 6 });
-	expect(editorState.selection.focus).toEqual({ key: 'first', offset: 11 });
-});
+		writeCommand.execute();
+		const executed = writeCommand.undo();
 
-it('should delete text inserted by execute and restore selection Range on multiple lines', () => {
-	const { editorState } = setup();
-	const writeCommand = new WriteCommand(editorState, { text: 'Z' });
-	editorState.selection.set({
-		anchor: { key: 'first', offset: 6 },
-		focus: { key: 'third', offset: 4 },
+		expect(executed).toBe(true);
+		expect(writeCommand.undoed).toBe(true);
+		expect(editorState.nodes.length).toBe(5);
+		expect(editorState.nodes[0].text).toBe('Lorem ipsum');
+		expect(editorState.nodes[1].text).toBe('dolor sit');
+		expect(editorState.nodes[2].text).toBe('bye bye');
+		expect(editorState.selection.anchor).toEqual({ key: 'first', offset: 6 });
+		expect(editorState.selection.focus).toEqual({ key: 'third', offset: 4 });
 	});
 
-	writeCommand.execute();
-	const executed = writeCommand.undo();
+	it('should delete text inserted by execute, restore text and selection on multiple lines (first line completely deleted)', () => {
+		const { editorState } = setup();
+		const writeCommand = new WriteCommand(editorState, { text: 'Z' });
+		editorState.selection.set({
+			anchor: { key: 'first', offset: 0 },
+			focus: { key: 'third', offset: 4 },
+		});
 
-	expect(executed).toBe(true);
-	expect(writeCommand.undoed).toBe(true);
-	expect(editorState.nodes.length).toBe(5);
-	expect(editorState.nodes[0].text).toBe('Lorem ipsum');
-	expect(editorState.nodes[1].text).toBe('dolor sit');
-	expect(editorState.nodes[2].text).toBe('bye bye');
-	expect(editorState.selection.anchor).toEqual({ key: 'first', offset: 6 });
-	expect(editorState.selection.focus).toEqual({ key: 'third', offset: 4 });
+		writeCommand.execute();
+		const executed = writeCommand.undo();
+
+		expect(executed).toBe(true);
+		expect(writeCommand.undoed).toBe(true);
+		expect(editorState.nodes.length).toBe(5);
+		expect(editorState.nodes[0].text).toBe('Lorem ipsum');
+		expect(editorState.nodes[1].text).toBe('dolor sit');
+		expect(editorState.nodes[2].text).toBe('bye bye');
+		expect(editorState.selection.anchor).toEqual({ key: 'first', offset: 0 });
+		expect(editorState.selection.focus).toEqual({ key: 'third', offset: 4 });
+	});
 });
